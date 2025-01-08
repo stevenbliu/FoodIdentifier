64edd443e815df02ceef2ff80f7fcbf9a0fbf572
stevenbliu <steventheliu@gmail.com>
2025-01-08 12:58:49 -0800

    update

--- backend/authentication/tests/test_views.py
+++ backend/authentication/tests/test_views.py
@@ -59,13 +59,12 @@ class AuthenticationViewsTestCase(APITestCase):
 
     def test_login_user_with_invalid_credentials(self):
         url = reverse('login')
-        WRONG_PASSWORD = 'wrong' + TEST_PASSWORD             # pragma: allowlist secret
 
 
         data = {
             'username': 'wrong' + TEST_USERNAME,
-            # pragma: allowlist secret
-            'password': WRONG_PASSWORD
+            'password': 'wrong' + TEST_PASSWORD # pragma: allowlist secret
+
         }
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
