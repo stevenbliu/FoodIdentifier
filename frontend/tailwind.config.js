f9376f000ee9ed5686ed18d7e19a253a1ad43d01
stevenbliu <steventheliu@gmail.com>
2025-02-12 15:59:12 -0800

    prepping environment to implement tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}', // Adjust according to your file structure
+      './frontend/**/*.{html,js,jsx,ts,tsx}', // Add paths where Tailwind classes are used
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
